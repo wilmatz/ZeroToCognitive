9310d55df7bc95a63b0369d3344acd307164cfc3
wilmatz <32905872+wilmatz@users.noreply.github.com>
2017-10-31 23:10:43 +0100

    first modifications

--- Chapter03/controller/restapi/features/speech_to_text.js
+++ Chapter03/controller/restapi/features/speech_to_text.js
@@ -4,5 +4,18 @@ var vcapServices = require('vcap_services');
 var config = require('../../env.json');
 
 exports.token = function(req, res) {
+    var sttConfig = extend(config.speech_to_text, vcapServices.getCredentials('speech_to_text'));
 
+    var sttAuthService = watson.authorization(sttConfig);
+
+    sttAuthService.getToken({
+        url: sttConfig.url
+    }, function(err, token) {
+        if (err) {
+            console.log('Error retrieving token: ', err);
+            res.status(500).send('Error retrieving token');
+            return;
+        }
+        res.send(token);
+    });
 }
